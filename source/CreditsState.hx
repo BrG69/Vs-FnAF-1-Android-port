36a10c777c8c781711bf715648a2f633ff279843
EmirPlays <96060395+EmirPlays@users.noreply.github.com>
2022-04-09 22:03:14 +0800

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -87,7 +87,8 @@ class CreditsState extends MusicBeatState
 			[''],
 			['Android Port'],
 			['Raiden Alfares',      'raiden',   'coding?',      'https://youtube.com/channel/UChE0s906J1YZRf1Ln9wP8Gg',   'FFFFF'], 
-			['BrG',       'brg',    'helper',      'https://youtube.com/c/BasicallyItsBrG',      'FFFFF']
+			['BrG',       'brg',    'helper',      'https://youtube.com/c/BasicallyItsBrG',      'FFFFF'], 
+			['EmirPlays',       'EmirPlays',    'tester',      'https://youtube.com/channel/UCANExMiyDRT5ITDSKyhKa3g',      'FFFFF']
 		];
 		
 		for(i in pisspoop){
